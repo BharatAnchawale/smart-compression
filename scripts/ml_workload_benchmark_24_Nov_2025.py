77a2263f9f566f0032ed4c80e08540835103d8e2
Bharat Anchawale <anchawalebharat203@gmail.com>
2025-11-28 16:25:34 +0530

    Updated eval script with brotli fix

--- scripts/ml_workload_benchmark_24_Nov_2025.py
+++ scripts/ml_workload_benchmark_24_Nov_2025.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12dd18fe79b6a3816b8aeb3ff01d7d5c99808d3584dbc757c4febaf8854c5568
-size 33466
+oid sha256:77d5b8a7aedf9a368b5eb95960f7c976066f93b3f3020e6ccbb04a32f3f4a696
+size 33434
