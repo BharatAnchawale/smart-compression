bf358e35953de026c8cfd80f75e8a7b6b35b8a72
Bharat Anchawale <anchawalebharat203@gmail.com>
2025-11-28 18:06:45 +0530

    Added smart_compression logic

---
+++ scripts/smart_compression_model_24_Nov_2025.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7202d24c576fa90e2bc8c31a78c71833d3188c3ed8040c53fed6be59b45ae1e9
+size 20886
---
+++ scripts/test_smart_compression.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f0c2f992593c414c7879b145b9d45b4925f9c4abb54f5e8f6ef8a0784ce0a59d
+size 14482
